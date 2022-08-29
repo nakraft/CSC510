7cd5495567ea1950a1b40c096ef99d0095dd0268
Parth-25m <partha.25m@gmail.com>
2022-08-29 01:40:38 -0400

    Created setup.py
    TODO:
        * fix license information in setup.py
        * Add fields as we progress
    Reference: https://github.com/bmcfee/spatialtree/blob/master/setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='csc510',
+    version='1',
+    description='CSC510 project',
+    author='Natalie Kraft, Ashrita Ramaswamy, Parth Sarthi',
+    packages=['csc510'],
+    long_description="""\
+        Project for CSC 510.
+      """,
+    classifiers=[
+        #"License :: OSI Approved :: GNU General Public License (GPL)",
+        "Development Status :: Alpha",
+        "Intended Audience :: Developers",
+    ],
+    keywords='csc510',
+    # license='GPL',
+    install_requires=[
+    ],
+)
