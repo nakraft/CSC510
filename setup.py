a31b5cd6630c06d590a75e8d17889d162be86f9f
Parth-25m <partha.25m@gmail.com>
2022-08-29 21:06:29 -0400

    [setup.py] : updated license and authors

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ setup(
     name='csc510',
     version='1',
     description='CSC510 project',
-    author='Natalie Kraft, Ashrita Ramaswamy, Parth Sarthi',
+    author='Natalie Kraft, Ashrita Ramaswamy, Parth Sarthi, Seungyoung Lee',
     packages=['csc510'],
     long_description="""\
         Project for CSC 510.
       """,
     classifiers=[
-        #"License :: OSI Approved :: GNU General Public License (GPL)",
+        "License :: OSI Approved :: MIT License",
         "Development Status :: Alpha",
         "Intended Audience :: Developers",
     ],
     keywords='csc510',
-    # license='GPL',
+    license='MIT',
     install_requires=[
     ],
 )
