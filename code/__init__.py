b02fc9a642fc9b317722eca81cd74ce5bacd4589
nakraft <99683342+nakraft@users.noreply.github.com>
2022-08-29 20:53:06 -0400

    Include test case

--- code/__init__.py
+++ code/__init__.py
@@ -1,3 +1,10 @@
 # use to define import order 
 # or to define the API
 # will complete once HW2-5 structure has been determined
+
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(3) == 5
