136f8a2bdef8872b207b033fe78a8301d6ec335f
Natalie Kraft <nakraft@ncsu.edu>
2022-09-07 22:40:45 -0400

    updated add method in data class

--- code/data.py
+++ code/data.py
@@ -1,7 +1,7 @@
-import math
-import random
-import re
-from .helper import per, the
+
+import row
+import cols
+from helper import lists 
 
 class Data:
 
@@ -11,13 +11,22 @@ class Data:
         self.src = src
 
         if isinstance(src, str): 
-            # do something 
-            print(src)
+            # calls the csv function which reads in a line of data 
+            lists.csv(src, Data.add(row)) # TODO: how do you pass a function with a variable yet to be declared
         else:
-            # do something else 
+            # or put in a table directly from src 
+            for key, value in enumerate(src): 
+                Data.add(value)
 
     def add(self, xs, row): 
-        # implement add function 
+        
+        if self.cols is not None: 
+            self.cols = cols(xs)
+        else: 
+            row = lists.push(self.rows, row(xs))
+            for key, value in enumerate(self.cols.x, self.cols.y): 
+                for key_inner, value_inner in enumerate(value): 
+                    cols.add(row.cells[value_inner.at]) # TODO: what is the equivalent of col.at in python
 
     def stats(self, places = 2, showCols = data.cols.x,  fun = 'mid', t, v): 
         # implement stats function 
